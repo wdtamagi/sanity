2c635a3b20b0247bf532ae39ee845b446582e91b
Marius Lundgård <studio@mariuslundgard.com>
2021-02-18 15:21:41 +0100

    refactor(form-builder): Migrate UnknownFields to `@sanity/ui`

--- packages/@sanity/form-builder/src/inputs/ObjectInput/ObjectInput.tsx
+++ packages/@sanity/form-builder/src/inputs/ObjectInput/ObjectInput.tsx
@@ -5,7 +5,7 @@ import Fieldset from 'part:@sanity/components/fieldsets/default'
 import PatchEvent, {set, setIfMissing, unset} from '../../PatchEvent'
 import isEmpty from '../../utils/isEmpty'
 import Field from './Field'
-import UnknownFields from './UnknownFields'
+import {UnknownFields} from './UnknownFields'
 import fieldStyles from './styles/Field.css'
 
 import styles from './styles/ObjectInput.css'
