ad44edfd3a3d7183132e281eb88a871ba2f02d62
Bjørge Næss <bjoerge@gmail.com>
2021-03-23 17:08:13 +0100

    refactor(form-builder): migrate ObjectInput to Sanity UI

--- packages/@sanity/form-builder/src/inputs/ObjectInput/Field.tsx
+++ packages/@sanity/form-builder/src/inputs/ObjectInput/Field.tsx
@@ -1,101 +1,93 @@
-import React from 'react'
-import Fieldset from 'part:@sanity/components/fieldsets/default'
+import React, {ForwardedRef, forwardRef, useCallback} from 'react'
+import {FormFieldSet} from '@sanity/base/components'
 import {resolveTypeName} from '@sanity/util/content'
+import {FormFieldPresence} from '@sanity/base/presence'
+import {Marker, Path, SchemaType} from '@sanity/types'
 import {FormBuilderInput} from '../../FormBuilderInput'
 import {InvalidValueInput} from '../InvalidValueInput'
-import styles from './styles/Field.css'
+import PatchEvent from '../../PatchEvent'
 
-type FieldProps = {
-  field: any
-  value?: any
-  compareValue?: any
-  onChange: (...args: any[]) => any
-  onFocus: (...args: any[]) => any
-  onBlur: (...args: any[]) => any
-  focusPath?: any[]
-  filterField?: (...args: any[]) => any
+interface FieldType {
+  name: string
+  type: SchemaType
+}
+interface FieldProps {
+  field: FieldType
+  value: unknown
+  compareValue: unknown
+  onChange: (event: PatchEvent, field: FieldType) => void
+  onFocus: (path: Path) => void
+  onBlur: () => void
+  focusPath?: Path
+  filterField?: (type: SchemaType) => boolean
   readOnly?: boolean
-  markers?: any[]
-  level?: number
-  presence: any
+  markers?: Marker[]
+  level: number
+  presence?: FormFieldPresence[]
 }
 // This component renders a single type in an object type. It emits onChange events telling the owner about the name of the type
 // that changed. This gives the owner an opportunity to use the same event handler function for all of its fields
-export default class Field extends React.PureComponent<FieldProps> {
-  _input: any
-  static defaultProps = {
-    level: 0,
-    focusPath: [],
-  }
-  handleChange = (event) => {
-    const {field, onChange} = this.props
-    if (!field.type.readOnly) {
-      onChange(event, field)
-    }
-  }
-  focus() {
-    if (this._input && typeof this._input.focus === 'function') {
-      this._input.focus()
-    }
-  }
-  setInput = (input) => {
-    this._input = input
-  }
-  render() {
-    const {
-      value,
-      readOnly,
-      field,
-      level,
-      onFocus,
-      onBlur,
-      markers,
-      focusPath,
-      filterField,
-      compareValue,
-      presence,
-    } = this.props
-    if (typeof value !== 'undefined') {
-      const expectedType = field.type.name
-      const actualType = resolveTypeName(value)
-      // todo: we should consider removing this, and not allow aliasing native types
-      // + ensure custom object types always gets annotated with _type
-      const isCompatible = actualType === field.type.jsonType
-      if (expectedType !== actualType && !isCompatible) {
-        return (
-          <div className={styles.root}>
-            <Fieldset legend={field.type.title} level={level} presence={presence}>
-              <InvalidValueInput
-                value={value}
-                onChange={this.handleChange}
-                validTypes={[field.type.name]}
-                actualType={actualType}
-                ref={this.setInput}
-              />
-            </Fieldset>
-          </div>
-        )
+export const Field = forwardRef(function Field(props: FieldProps, forwardedRef: ForwardedRef<any>) {
+  const {
+    value,
+    readOnly,
+    field,
+    level,
+    onChange,
+    onFocus,
+    onBlur,
+    markers,
+    focusPath,
+    filterField,
+    compareValue,
+    presence,
+  } = props
+
+  const handleChange = useCallback(
+    (event) => {
+      if (!field.type.readOnly) {
+        onChange(event, field)
       }
+    },
+    [onChange, field]
+  )
+
+  if (typeof value !== 'undefined') {
+    const expectedType = field.type.name
+    const actualType = resolveTypeName(value)
+    // todo: we should consider removing this, and not allow aliasing native types
+    // + ensure custom object types always gets annotated with _type
+    const isCompatible = actualType === field.type.jsonType
+    if (expectedType !== actualType && !isCompatible) {
+      return (
+        <FormFieldSet title={field.type.title} level={level} __unstable_presence={presence}>
+          <InvalidValueInput
+            value={value}
+            onChange={handleChange}
+            validTypes={[field.type.name]}
+            actualType={actualType}
+            ref={forwardedRef}
+          />
+        </FormFieldSet>
+      )
     }
-    return (
-      <div className={styles.root}>
-        <FormBuilderInput
-          value={value}
-          type={field.type}
-          onChange={this.handleChange}
-          path={[field.name]}
-          onFocus={onFocus}
-          onBlur={onBlur}
-          readOnly={readOnly || field.type.readOnly}
-          focusPath={focusPath}
-          filterField={filterField}
-          markers={markers}
-          compareValue={compareValue}
-          level={level}
-          presence={presence}
-          ref={this.setInput}
-        />
-      </div>
-    )
   }
-}
+  return (
+    <FormBuilderInput
+      value={value}
+      type={field.type}
+      onChange={handleChange}
+      path={[field.name]}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      readOnly={readOnly || field.type.readOnly}
+      focusPath={focusPath}
+      filterField={filterField}
+      markers={markers}
+      compareValue={compareValue}
+      level={level}
+      presence={presence}
+      ref={forwardedRef}
+    />
+  )
+})
---
+++ packages/@sanity/form-builder/src/inputs/ObjectInput/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const FieldWrapper = styled.div`
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  min-width: 0;
+`
